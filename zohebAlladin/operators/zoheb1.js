d9e162c2d7bcdf49017b5ff29fd817d2b70852a0
zoheballadin <beanzoheb@gmail.com>
2022-11-24 12:47:29 +0530

    BONUS QUESTION BY ZOHEB WEEK 3

---
+++ zohebAlladin/operators/zoheb1.js
@@ -0,0 +1,13 @@
+var a = 4804, b = 9991;
+
+if(a&1 == 1)
+console.log(`a = ${a} is an odd number`);
+else
+console.log(`a = ${a} is an even number`);
+
+if(b&1 == 1)
+console.log(`b = ${b} is an odd number`);
+else
+console.log(`b = ${b} is an even number`);
+
+
---
+++ zohebAlladin/operators/zoheb3.js
@@ -0,0 +1,4 @@
+var a = 2;
+var power = 16;
+var ans = Math.pow(a,power);
+console.log(`${a} to the power of ${power} is ${ans}`);

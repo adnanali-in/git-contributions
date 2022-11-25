081c59662a179f90ed1be5fada9a41569c050112
amish-j <amishjewani6@gmail.com>
2022-11-25 16:05:38 +0530

    this is a commit by amishJ

---
+++ amishJewani/operators.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = 2;
+console.log("sum of a + b is: ")
+console.log( a + b );
+
+console.log(a++)
+console.log(b++)
+
+
+//comparions 
+var a = 100;
+var b = 101;
+console.log(a>b);
+
+console.log(typeof(a));
